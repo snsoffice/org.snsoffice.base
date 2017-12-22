1056647ffd0993d2dd30b34277ce0a81a71db419
Jondy Zhao <jondy.zhao@gmail.com>
2017-12-22 19:14:17 +0800

    Add content types

---
+++ src/org/snsoffice/base/geo.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+from org.snsoffice.base import _
+
+from plone.autoform.interfaces import IFormFieldProvider
+from plone.autoform.directives import form
+from plone.supermodel import model
+from zope.interface import provider
+
+
+@provider(IFormFieldProvider)
+class IGeoFeature(model.Schema):
+    """GeoFeature interface to store geometry and style of feature.
+    """
+
+    form.fieldset(
+        'geofeature',
+        label=_(u"label_tab_geofeture", default=u'Geography'),
+        fields=['geometry', 'geostyle']
+    )
+
+    geometry = schema.TextLine(
+        title=_(u'label_geometry', default=u'Geometry'),
+        description=_(u"Feature geometry in WKT format"),
+        required=False,
+    )
+
+    geostyle = schema.TextLine(
+        title=_(u'label_geostyle', default=u'Style'),
+        description=_(u"Feature style in JSON format"),
+        required=False,
+    )
--- src/org/snsoffice/base/interfaces.py
+++ src/org/snsoffice/base/interfaces.py
@@ -6,3 +6,24 @@ from zope.publisher.interfaces.browser import IDefaultBrowserLayer
 
 class IOrgSnsofficeBaseLayer(IDefaultBrowserLayer):
     """Marker interface that defines a browser layer."""
+
+class IHouse(model.Schema):
+    """Schema for House content type."""
+    
+    anchor = schema.TextLine(title=_(u'Username of the anchor'))
+    source = schema.URL(title=_(u'Source uri of this house'))
+
+    # def getAnchor():
+    # """Get anchor of this house, None if no available anchor."""
+
+class IOrganization(IHouse):
+    """Schema for Organization content type."""
+
+class IBuilding(IHouse):
+    """Schema for Building content type."""
+
+class IFloor(IHouse):
+    """Schema for Floor content type."""
+
+class IRoom(IHouse):
+    """Schema for Room content type."""
