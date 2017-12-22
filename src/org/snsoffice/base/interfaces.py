b9be13b61b1ea409c721687c497570dbdcffc256
Jondy Zhao <jondy.zhao@gmail.com>
2017-12-22 19:21:06 +0800

    Fix bug

--- src/org/snsoffice/base/interfaces.py
+++ src/org/snsoffice/base/interfaces.py
@@ -2,7 +2,7 @@
 """Module where all interfaces, events and exceptions live."""
 
 from zope.publisher.interfaces.browser import IDefaultBrowserLayer
-
+from plone.supermodel import model
 
 class IOrgSnsofficeBaseLayer(IDefaultBrowserLayer):
     """Marker interface that defines a browser layer."""
