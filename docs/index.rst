62d997234d3d951689643e8090b4a4eea544a478
Jondy Zhao <jondy.zhao@gmail.com>
2017-12-23 07:20:12 +0800

    Refine code

--- docs/index.rst
+++ docs/index.rst
@@ -2,6 +2,28 @@
 org.snsoffice.base
 ====================
 
+
+Plone 5.0.4 问题
+================
+
+* 导航窗口没有显示自定义类型
+
 网站设置=>导航
 
 显示内容类型下选中： Organization, Building, Floor, Room
+
+* 新增菜单中查询集没有对齐问题
+
+Fix collection menu item error indentation in factory menu It changed
+by default.css and use !important
+
+    #collection.contenttype-collection {
+        padding-left: 50px !important;
+    }
+
+* 部分内容没有汉化问题
+
+拷贝 snsgis 里面的 plone.mo 和 widgets.mo，这是以前修改好的
+
+  plone.app.locales-5.0.9-py2.7.egg/plone/app/locales/locales/zh_CN/LC_MESSAGES/plone.mo
+  plone.app.locales-5.0.9-py2.7.egg/plone/app/locales/locales/zh_CN/LC_MESSAGES/widgets.mo
--- src/org/snsoffice/base/browser/helper.py
+++ src/org/snsoffice/base/browser/helper.py
@@ -3,6 +3,14 @@ from org.snsoffice.base import _
 
 from Products.Five.browser import BrowserView
 from plone.app.content.utils import json_dumps
+from plone.app.contenttypes.browser.collection import CollectionView
+from Products.CMFPlone.browser.search import Search
+from Products.CMFPlone.PloneBatch import Batch
+from Products.CMFPlone.browser.navtree import getNavigationRoot
+from plone.registry.interfaces import IRegistry
+from zope.component import getMultiAdapter
+from zope.component import queryUtility
+from zope.component import getUtility
 
 class AnchorHelperView(BrowserView):
     """A simple view to get anchors of one house."""
@@ -11,4 +19,73 @@ class AnchorHelperView(BrowserView):
         self.request.response.setHeader("Content-Type", "application/json")
         return json_dumps({
             'anchors': self.context.anchors
+        })
+
+class AjaxHouseSearch(Search):
+    """SearchableText, portal_type, path, show_inactive, sort_on, sort_order, sort_limit"""
+
+    def __call__(self):
+        items = []
+        try:
+            per_page = int(self.request.form.get('perPage'))
+        except:
+            per_page = 10
+        try:
+            page = int(self.request.form.get('page'))
+        except:
+            page = 1
+
+        results = self.results(batch=False, use_content_listing=False)
+        batch = Batch(results, per_page, start=(page - 1) * per_page)
+
+        registry = queryUtility(IRegistry)
+        length = registry.get('plone.search_results_description_length')
+        plone_view = getMultiAdapter(
+            (self.context, self.request), name='plone')
+        registry = getUtility(IRegistry)
+        view_action_types = registry.get(
+            'plone.types_use_view_action_in_listings', [])
+        for item in batch:
+            url = item.getURL()
+            if item.portal_type in view_action_types:
+                url = '%s/view' % url
+            items.append({
+                'id': item.UID,
+                'title': item.Title,
+                'description': plone_view.cropText(item.Description, length),
+                'url': url,
+                'state': item.review_state if item.review_state else None,
+            })
+        return json.dumps({
+            'total': len(results),
+            'items': items
+        })
+
+class CollectionHouseView(CollectionView):
+
+    def __call__(self):
+        items = []
+        batch = self.batch()
+
+        registry = queryUtility(IRegistry)
+        length = registry.get('plone.search_results_description_length')
+        plone_view = getMultiAdapter(
+            (self.context, self.request), name='plone')
+        registry = getUtility(IRegistry)
+        view_action_types = registry.get(
+            'plone.types_use_view_action_in_listings', [])
+        for item in batch:
+            url = item.getURL()
+            if item.portal_type in view_action_types:
+                url = '%s/view' % url
+            items.append({
+                'id': item.UID,
+                'title': item.Title,
+                'description': plone_view.cropText(item.Description, length),
+                'url': url,
+                'state': item.review_state if item.review_state else None,
+            })
+        return json.dumps({
+            'total': len(results),
+            'items': items
         })
